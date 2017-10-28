52c556c86c6c0ec6ba26b569ada73b2aea90e8f7
amatranga <amatranga892@gmail.com>
2017-10-28 13:08:08 -0700

    Implement NavControl component

--- client/src/components/NavControl.jsx
+++ client/src/components/NavControl.jsx
@@ -3,17 +3,22 @@ import React from 'react';
 const NavControl = () => {
   const user = window.user;
 
-  if (user) {
+  if (!user) {
     return (
       <div className="navbar-nav mr-auto">
         <a className="nav-item nav-link" href="/about">About</a>
         <a className="nav-item nav-link" href="/Request">Request</a>
-        <a className="nav-item nav-link" href="/profile">My Profile</a>
+        <form className="form-inline my2 my-lg-0">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search by Username" aria-label="Search by Username" />
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
       </div>
     );
   } else {
     return (
-      <div className="navbar-nav mr-auto"></div>
+      <div className="navbar-nav mr-auto">
+         <a className="nav-item nav-link" href="/about">About</a>
+      </div>
     );
   }
 };
