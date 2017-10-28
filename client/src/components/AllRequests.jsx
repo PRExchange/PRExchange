afefdc2d3af6c931c23c1dc83901fe0b5e363e32
amatranga <amatranga892@gmail.com>
2017-10-28 15:35:11 -0700

    Implement simple AllRequests component
    This will be where the user sees all other user requests

---
+++ client/src/components/AllRequests.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class AllRequests extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        AllRequests Component
+      </div>
+    );
+  }
+};
+
+export default AllRequests;
