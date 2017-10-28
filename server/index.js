7e41f6ecb1d21eee737adb794a4803e45e37eef3
amatranga <amatranga892@gmail.com>
2017-10-28 11:42:54 -0700

    Initial setup

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+// const db = require('../db');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`PRExchange is running on port ${PORT}`);
+});
