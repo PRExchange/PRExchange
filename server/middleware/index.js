d7cda0c6e59f4e18f8f187abef2cbe09a0feb992
amatranga <amatranga892@gmail.com>
2017-10-28 18:06:28 -0700

    Requrie in development dependencies

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,1 +1,6 @@
 module.exports.auth = require('./auth');
+module.exports.passport = require('./passport');
+module.exports.bodyParser = require('body-parser');
+module.exports.cookieParser = require('cookie-parser');
+module.exports.flash = require('connect-flash');
+module.exports.morgan = require('morgan');
