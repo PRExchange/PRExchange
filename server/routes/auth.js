072c0edeb59a508a437303ec622a4ac1f0fcc24f
amatranga <amatranga892@gmail.com>
2017-10-31 20:06:35 -0700

    Implement About page routing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,13 +6,24 @@ const router = express.Router();
 router.route('/')
   .get(middleware.auth.homeRedirect, middleware.auth.render);
 
+router.route('/about')
+  .get(middleware.auth.render);
+
 router.route('/home')
   .get(middleware.auth.verify, middleware.auth.render);
 
 router.route('/login')
   .get((req, res) => {
     res.render('login.ejs')
-  });
+  }
+);
+
+router.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  }
+);
 
 router.get('/auth/github', middleware.passport.authenticate('github', {
   scope: ['user:email']
