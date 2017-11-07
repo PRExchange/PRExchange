cfe6f4311909bb27ef5a0e13e5ff01db0ed8b306
amatranga <amatranga892@gmail.com>
2017-11-07 15:26:32 -0800

    Implement routing for API

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/')
-  .get((req, res, next) => {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.status(403).send('You must be authenticated to use the API');
+  .get((req, res) => {
+    res.status(200).send('Hello, World!');
+  })
+  .post((req, res) => {
+    res.status(201).send({ data: 'Posted!' });
   });
 
 module.exports = router;
