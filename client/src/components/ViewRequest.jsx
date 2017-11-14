3851eb0d22120cd2c77edd51fdbf33257071bfa4
amatranga <amatranga892@gmail.com>
2017-11-13 18:23:22 -0800

    Begin to implement ViewRequest
    WIP

---
+++ client/src/components/ViewRequest.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ViewRequest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        ViewRequest component!
+      </div>
+    );
+  }
+};
+
+export default ViewRequest;
